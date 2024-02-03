2d6282f19132e3c0d19119ab955cee8306a6a62b
Caen De Silva <caen@desilva.se>
2024-02-03 19:43:25 +0100

    Center header

--- resources/views/windowlight.blade.php
+++ resources/views/windowlight.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <header class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6">
+            <header class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6 text-center">
                 <div class="mb-4">
                     <h1 class="font-semibold text-2xl text-gray-800 dark:text-gray-200 leading-tight mb-4">
                         Welcome to Windowlight.
