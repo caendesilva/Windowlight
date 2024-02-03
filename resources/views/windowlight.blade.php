ab2fe38963cff40532f0fa9fb810ee2aa69519f9
Caen De Silva <caen@desilva.se>
2024-02-03 18:35:46 +0100

    Add header

--- resources/views/windowlight.blade.php
+++ resources/views/windowlight.blade.php
@@ -1,11 +1,16 @@
 <x-app-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    Welcome!
+            <header class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6">
+                    <h1 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight mb-4">
+                        Welcome to Windowlight.
+                    </h1>
+                    <strong class="text-gray-800 dark:text-gray-200">
+                        Windowlight is a simple wrapper for Torchlight, helping you to create beautiful code screenshots.
+                    </strong>
                 </div>
-            </div>
+            </header>
         </div>
     </div>
 </x-app-layout>
