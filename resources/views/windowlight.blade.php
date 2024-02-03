3e25ad42d715cb29e6773f0485ce80dfef08ef66
Caen De Silva <caen@desilva.se>
2024-02-03 19:43:15 +0100

    Improved frontend

--- resources/views/windowlight.blade.php
+++ resources/views/windowlight.blade.php
@@ -10,13 +10,24 @@
                         Windowlight is a simple wrapper for Torchlight, helping you to create beautiful code screenshots.
                     </strong>
                 </div>
+            </header>
+        </div>
 
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-8">
+            <section class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6">
                 <div class="text-gray-900 dark:text-gray-100">
                     <form method="POST" action="{{ route('windowlight.store') }}">
+                        <header class="mb-4">
+                            <label for="code">
+                                <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                                    Enter code
+                                </h2>
+                            </label>
+                        </header>
+
                         @csrf
 
                         <div class="mb-4">
-                            <x-input-label for="code" :value="__('Enter code')" />
                             <x-textarea class="block mt-1 w-full" name="code" rows="8" required autofocus>{{ $example }}</x-textarea>
                             <x-input-error :messages="$errors->get('code')" class="mt-2" />
                         </div>
@@ -26,11 +37,22 @@
                         </x-primary-button>
                     </form>
                 </div>
-            </header>
-
-            @if($result)
-                @include('windowlight.result')
-            @endif
+            </section>
         </div>
+
+        @if($result)
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-8">
+                <section class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6">
+                    <header>
+                        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                            Code result
+                        </h2>
+                    </header>
+                    <div>
+                        @include('windowlight.result')
+                    </div>
+                </section>
+            </div>
+        @endif
     </div>
 </x-app-layout>
