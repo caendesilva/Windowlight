7b9ec720bf9c898ef928c516b1c34bf90ddee5b4
Caen De Silva <caen@desilva.se>
2024-02-03 19:37:22 +0100

    Create base code result window

--- resources/views/windowlight/result.blade.php
+++ resources/views/windowlight/result.blade.php
@@ -1,3 +1,34 @@
 @props(['result'])
 
-{!! $result !!}
+@php
+$padding = 1; // Rem
+$width = 64; // Ch
+@endphp
+
+{{-- To improve compatability we use non-semantic elements, meaning divs everywhere, as well as inline styles. --}}
+
+<style>
+    #code-card-wrapper {
+        /* Dynamic styles */
+        padding: {{ $padding }}rem;
+        width: {{ $width }}ch;
+
+        /* Static styles */
+        resize: horizontal;
+        overflow: hidden;
+        max-width: 100%;
+    }
+
+    #code-card-wrapper:hover {
+        /* Show the screenshot bounding box when highlighted */
+        outline: rgba(255, 255, 255, 0.5) solid;
+    }
+</style>
+
+<div id="code-card-wrapper">
+    <div id="code-card">
+        <div id="torchlight-wrapper">
+            {!! $result !!}
+        </div>
+    </div>
+</div>
