266af52535518c330735b9eff279b552f3dccffc
Caen De Silva <caen@desilva.se>
2024-02-03 18:27:40 +0100

    Add home route

--- app/Http/Controllers/WindowlightController.php
+++ app/Http/Controllers/WindowlightController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class WindowlightController extends Controller
 {
-    //
+    public function show()
+    {
+        return view('windowlight');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\WindowlightController::class, 'show'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
