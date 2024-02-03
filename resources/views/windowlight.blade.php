cbdc9ac9cd0c341d1c0d7472476f4a0d504111c1
Caen De Silva <caen@desilva.se>
2024-02-03 20:36:01 +0100

    Serve asset locally

--- resources/views/windowlight.blade.php
+++ resources/views/windowlight.blade.php
@@ -118,5 +118,5 @@
         }
     </script>
 
-    <script defer src="https://cdn.jsdelivr.net/npm/html2canvas@1.4.1/dist/html2canvas.min.js"></script>
+    <script defer src="{{ asset('vendor/html2canvas.min.js') }}"></script>
 </x-app-layout>
