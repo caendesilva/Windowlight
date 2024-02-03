735a32c870d48417faff6114e7d9b1111b23b180
Caen De Silva <caen@desilva.se>
2024-02-03 19:58:05 +0100

    Row layout on large devices

--- resources/views/windowlight.blade.php
+++ resources/views/windowlight.blade.php
@@ -13,47 +13,49 @@
             </header>
         </div>
 
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-8">
-            <section class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6">
-                <div class="text-gray-900 dark:text-gray-100">
-                    <form method="POST" action="{{ route('windowlight.store') }}">
-                        <header class="mb-4">
-                            <label for="code">
-                                <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                                    Enter code
-                                </h2>
-                            </label>
-                        </header>
-
-                        @csrf
-
-                        <div class="mb-4">
-                            <x-textarea class="block mt-1 w-full" name="code" rows="8" required autofocus>{{ $input }}</x-textarea>
-                            <x-input-error :messages="$errors->get('code')" class="mt-2" />
-                        </div>
-
-                        <x-primary-button type="submit">
-                            {{ __('Generate') }}
-                        </x-primary-button>
-                    </form>
-                </div>
-            </section>
-        </div>
-
-        @if($result)
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-8">
+        <div class="max-w-7xl mx-auto flex flex-wrap flex-col lg:flex-row sm:px-3 lg:px-5">
+            <div class="w-full lg:w-1/2 mx-auto px-3 mt-8">
                 <section class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6">
-                    <header>
-                        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                            Code result
-                        </h2>
-                    </header>
-                    <div>
-                        @include('windowlight.result')
+                    <div class="text-gray-900 dark:text-gray-100">
+                        <form method="POST" action="{{ route('windowlight.store') }}">
+                            <header class="mb-4">
+                                <label for="code">
+                                    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                                        Enter code
+                                    </h2>
+                                </label>
+                            </header>
+
+                            @csrf
+
+                            <div class="mb-4">
+                                <x-textarea class="block mt-1 w-full" name="code" rows="8" required autofocus>{{ $input }}</x-textarea>
+                                <x-input-error :messages="$errors->get('code')" class="mt-2" />
+                            </div>
+
+                            <x-primary-button type="submit">
+                                {{ __('Generate') }}
+                            </x-primary-button>
+                        </form>
                     </div>
                 </section>
             </div>
-        @endif
+
+            @if($result)
+                <div class="w-full lg:w-1/2 mx-auto px-3 mt-8">
+                    <section class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6">
+                        <header>
+                            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                                Code result
+                            </h2>
+                        </header>
+                        <div>
+                            @include('windowlight.result')
+                        </div>
+                    </section>
+                </div>
+            @endif
+        </div>
     </div>
 
     <script>
