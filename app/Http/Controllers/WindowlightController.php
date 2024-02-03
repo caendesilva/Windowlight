f43ea61aff7b57b362b35d9753f66a3fc33fa10f
Caen De Silva <caen@desilva.se>
2024-02-03 18:44:38 +0100

    Create base store logic

--- app/Http/Controllers/WindowlightController.php
+++ app/Http/Controllers/WindowlightController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\CreateImageRequest;
 
 class WindowlightController extends Controller
 {
     public function show()
     {
         return view('windowlight');
+    }
+
+    public function store(CreateImageRequest $request)
+    {
+        $validated = $request->validated();
+
+        // Todo: Get code snippet
+        $result = $validated['code'];
+
+        return redirect()->route('home');
     }
 }
