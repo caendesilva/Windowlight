e815c72bc4a6280886d86b6936f675635df7dc98
Caen De Silva <caen@desilva.se>
2024-02-04 19:23:59 +0100

    Add placeholder

--- resources/views/windowlight.blade.php
+++ resources/views/windowlight.blade.php
@@ -112,7 +112,7 @@
                                 </div>
                                 <div class="mb-4">
                                     <x-input-label for="headerText" value="Header text" />
-                                    <x-text-input id="headerText" name="headerText" type="text" value="{{ $headerText }}" class="w-48" />
+                                    <x-text-input id="headerText" name="headerText" type="text" value="{{ $headerText }}" placeholder="App\Windowlight.php" class="w-48" />
                                     <x-input-error :messages="$errors->get('headerText')" class="mt-2" />
                                 </div>
                                 <div class="mb-4">
