1a50512b65ebf4616963c9abe146bbeacd134e9a
Caen De Silva <caen@desilva.se>
2024-02-04 20:05:22 +0100

    Update header semantics

--- resources/views/windowlight.blade.php
+++ resources/views/windowlight.blade.php
@@ -14,9 +14,11 @@
                     <h1 class="font-semibold text-2xl text-gray-800 dark:text-gray-200 leading-tight mb-4">
                         Welcome to Windowlight.
                     </h1>
-                    <strong class="text-base text-gray-800 dark:text-gray-200">
-                        Windowlight is a simple wrapper for Torchlight, helping you to create beautiful code screenshots.
-                    </strong>
+                    <p class="text-base text-gray-800 dark:text-gray-200">
+                        <strong>
+                            Windowlight is a simple wrapper for Torchlight, helping you to create beautiful code screenshots.
+                        </strong>
+                    </p>
                     <noscript>
                         <p class="text-sm text-red-800 dark:text-red-500 mt-2">
                             You need to enable JavaScript to be able to download the code screenshot.
