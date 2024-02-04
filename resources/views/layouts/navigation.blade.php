d8187ee7cc83a03f1603ffa994e83e93b6366544
Caen De Silva <caen@desilva.se>
2024-02-04 22:32:03 +0100

    Replace login with about

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,3 +1,4 @@
+@props(['replaceLoginWithAbout' => true])
 <nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -22,48 +23,54 @@
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
-                <x-dropdown align="right" width="48">
-                    <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                            <div>{{ Auth::user()?->name ?? 'Guest' }}</div>
+                @if($replaceLoginWithAbout && !Auth::check())
+                    <x-nav-link :href="route('about')" :active="request()->routeIs('about')">
+                        {{ __('About') }}
+                    </x-nav-link>
+                @else
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                                <div>{{ Auth::user()?->name ?? 'Guest' }}</div>
 
-                            <div class="ms-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                            </div>
-                        </button>
-                    </x-slot>
+                                <div class="ms-1">
+                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                    </svg>
+                                </div>
+                            </button>
+                        </x-slot>
 
-                    <x-slot name="content">
-                        @auth()
-                            <x-dropdown-link :href="route('profile.edit')">
-                                {{ __('Profile') }}
-                            </x-dropdown-link>
-
-                            <!-- Authentication -->
-                            <form method="POST" action="{{ route('logout') }}">
-                                @csrf
-
-                                <x-dropdown-link :href="route('logout')"
-                                                 onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                    {{ __('Log Out') }}
+                        <x-slot name="content">
+                            @auth()
+                                <x-dropdown-link :href="route('profile.edit')">
+                                    {{ __('Profile') }}
                                 </x-dropdown-link>
-                            </form>
-                        @endauth
-                        @guest()
-                            <x-dropdown-link :href="route('login')">
-                                {{ __('Log in') }}
-                            </x-dropdown-link>
-                            @if (Route::has('register'))
-                                <x-dropdown-link :href="route('register')">
-                                    {{ __('Register') }}
+
+                                <!-- Authentication -->
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+
+                                    <x-dropdown-link :href="route('logout')"
+                                                     onclick="event.preventDefault();
+                                                    this.closest('form').submit();">
+                                        {{ __('Log Out') }}
+                                    </x-dropdown-link>
+                                </form>
+                            @endauth
+                            @guest()
+                                <x-dropdown-link :href="route('login')">
+                                    {{ __('Log in') }}
                                 </x-dropdown-link>
-                            @endif
-                        @endguest
-                    </x-slot>
-                </x-dropdown>
+                                @if (Route::has('register'))
+                                    <x-dropdown-link :href="route('register')">
+                                        {{ __('Register') }}
+                                    </x-dropdown-link>
+                                @endif
+                            @endguest
+                        </x-slot>
+                    </x-dropdown>
+                @endif
             </div>
 
             <!-- Hamburger -->
@@ -115,9 +122,15 @@
                     </form>
                 @endauth
                 @guest()
-                    <x-responsive-nav-link :href="route('login')">
-                        {{ __('Log in') }}
-                    </x-responsive-nav-link>
+                    @if($replaceLoginWithAbout)
+                        <x-responsive-nav-link :href="route('about')">
+                            {{ __('About') }}
+                        </x-responsive-nav-link>
+                    @else
+                        <x-responsive-nav-link :href="route('login')">
+                            {{ __('Log in') }}
+                        </x-responsive-nav-link>
+                    @endif
                     @if (Route::has('register'))
                         <x-responsive-nav-link :href="route('register')">
                             {{ __('Register') }}
