2c5f1f991be78fe6435facb5b688edbbb50c8212
Caen De Silva <caen@desilva.se>
2024-02-04 16:58:58 +0100

    Remove margin

--- resources/views/windowlight.blade.php
+++ resources/views/windowlight.blade.php
@@ -10,7 +10,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <header class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6 text-center">
-                <div class="mb-4">
+                <div>
                     <h1 class="font-semibold text-2xl text-gray-800 dark:text-gray-200 leading-tight mb-4">
                         Welcome to Windowlight.
                     </h1>
