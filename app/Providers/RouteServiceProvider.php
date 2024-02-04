9c71f680b2cb7d90d7866515ee3e6df0271322b4
Caen De Silva <caen@desilva.se>
2024-02-04 22:02:16 +0100

    Use proper multiple rate limit support
    https://laravel.com/docs/10.x/routing#multiple-rate-limits

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -29,7 +29,10 @@ class RouteServiceProvider extends ServiceProvider
         });
 
         RateLimiter::for('generate', function (Request $request) {
-            return Limit::perMinute(30)->by($request->ip());
+            return [
+                Limit::perMinute(30)->by($request->ip()),
+                Limit::perDay(1000)->by($request->ip()),
+            ];
         });
 
         $this->routes(function () {
