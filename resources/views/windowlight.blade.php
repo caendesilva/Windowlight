4a23c49d579449a7af011451e6aec8d47e8b300a
Caen De Silva <caen@desilva.se>
2024-02-04 22:49:38 +0100

    Revert "Add todo"
    This reverts commit 26fed7b7c59407a847bbe51ebcd193094aa3d415.

--- resources/views/windowlight.blade.php
+++ resources/views/windowlight.blade.php
@@ -256,7 +256,6 @@
 
             let value = backgroundInput.value;
 
-            // Todo: If we support color names, we need to make sure they are hexadecimal before we reach this step
             if (!value.startsWith('#') && (value.length === 6 || value.length === 3)) {
                 value = `#${value}`;
             }
