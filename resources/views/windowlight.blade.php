c027e949883d2566362f46d67ad29a85187cdbf4
Caen De Silva <caen@desilva.se>
2024-02-15 10:52:10 +0100

    Format options markup into fieldset

--- resources/views/windowlight.blade.php
+++ resources/views/windowlight.blade.php
@@ -117,30 +117,39 @@
                                     </div>
 
                                     <div>
-                                        <div class="mb-4">
-                                            <label class="font-medium text-base text-gray-700 dark:text-gray-300 mb-1 inline-flex items-center cursor-pointer">
-                                                <span class="me-3 ">Line numbers</span>
-                                                <input type="checkbox" value="on" {{ $lineNumbers ? 'checked' : '' }} name="lineNumbers" id="lineNumbers" class="sr-only peer">
-                                                <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                                            </label>
-                                            <x-input-error :messages="$errors->get('lineNumbers')" class="mt-2" />
-                                        </div>
-                                        <div class="mb-4">
-                                            <label class="font-medium text-base text-gray-700 dark:text-gray-300 mb-1 inline-flex items-center cursor-pointer">
-                                                <span class="me-3 ">Show menu bar</span>
-                                                <input type="checkbox" value="on" {{ $useHeader ? 'checked' : '' }} name="useHeader" id="useHeader" class="sr-only peer">
-                                                <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                                            </label>
-                                            <x-input-error :messages="$errors->get('useHeader')" class="mt-2" />
-                                        </div>
-                                        <div class="mb-4">
-                                            <label class="font-medium text-base text-gray-700 dark:text-gray-300 mb-1 inline-flex items-center cursor-pointer">
-                                                <span class="me-3 ">Show menu buttons</span>
-                                                <input type="checkbox" value="on" {{ $headerButtons ? 'checked' : '' }} name="headerButtons" id="headerButtons" class="sr-only peer">
-                                                <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                                            </label>
-                                            <x-input-error :messages="$errors->get('headerButtons')" class="mt-2" />
-                                        </div>
+                                        <fieldset>
+                                            <legend>
+                                                <x-input-label>
+                                                    <strong>Options</strong>
+                                                </x-input-label>
+                                            </legend>
+                                            <ul class="ml-4">
+                                                <li class="mb-2">
+                                                    <label class="font-medium text-base text-gray-700 dark:text-gray-300 mb-1 inline-flex items-center cursor-pointer">
+                                                        <span class="me-3 ">Line numbers</span>
+                                                        <input type="checkbox" value="on" {{ $lineNumbers ? 'checked' : '' }} name="lineNumbers" id="lineNumbers" class="sr-only peer">
+                                                        <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                                                    </label>
+                                                    <x-input-error :messages="$errors->get('lineNumbers')" class="mt-2" />
+                                                </li>
+                                                <li class="mb-2">
+                                                    <label class="font-medium text-base text-gray-700 dark:text-gray-300 mb-1 inline-flex items-center cursor-pointer">
+                                                        <span class="me-3 ">Show menu bar</span>
+                                                        <input type="checkbox" value="on" {{ $useHeader ? 'checked' : '' }} name="useHeader" id="useHeader" class="sr-only peer">
+                                                        <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                                                    </label>
+                                                    <x-input-error :messages="$errors->get('useHeader')" class="mt-2" />
+                                                </li>
+                                                <li class="mb-2">
+                                                    <label class="font-medium text-base text-gray-700 dark:text-gray-300 mb-1 inline-flex items-center cursor-pointer">
+                                                        <span class="me-3 ">Show menu buttons</span>
+                                                        <input type="checkbox" value="on" {{ $headerButtons ? 'checked' : '' }} name="headerButtons" id="headerButtons" class="sr-only peer">
+                                                        <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                                                    </label>
+                                                    <x-input-error :messages="$errors->get('headerButtons')" class="mt-2" />
+                                                </li>
+                                            </ul>
+                                        </fieldset>
                                     </div>
                                 </div>
                             </fieldset>
