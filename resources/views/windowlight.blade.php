6734eaba25c3ae4e4a505f2e2ff5a1e59f0055d2
Caen De Silva <caen@desilva.se>
2024-02-15 10:53:20 +0100

    Add Oxford comma

--- resources/views/windowlight.blade.php
+++ resources/views/windowlight.blade.php
@@ -185,7 +185,7 @@
             <footer class="px-6 mt-12 -mb-4 text-center">
                 <p class="text-gray-800 dark:text-gray-200 leading-tight">
                     Built by <a href="https://twitter.com/CodeWithCaen" rel="author" class="text-indigo-700 dark:text-indigo-300 font-semibold">Caen De Silva</a>
-                    using <b>Laravel</b>, <b>TailwindCSS</b> and vanilla <b>JavaScript</b>.
+                    using <b>Laravel</b>, <b>TailwindCSS</b>, and vanilla <b>JavaScript</b>.
                 </p>
                 <p class="text-gray-800 dark:text-gray-200 leading-tight mt-2">
                     Syntax highlighting beautifully provided by <a href="https://torchlight.dev" rel="nofollow noopener" class="text-indigo-700 dark:text-indigo-300 font-semibold">Torchlight</a>.
