c92cc978c3970c9425b39186e5a238ae7f171664
Caen De Silva <caen@desilva.se>
2024-03-21 13:20:09 +0100

    Move view logic to new controller

--- app/Http/Controllers/MarkdownViewController.php
+++ app/Http/Controllers/MarkdownViewController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\HtmlString;
+use Illuminate\Support\Str;
 
 class MarkdownViewController extends Controller
 {
-    //
+    public function about()
+    {
+        return view('about', [
+            'markdown' => new HtmlString(Str::markdown(file_get_contents(resource_path('markdown/about.md'))))
+        ]);
+    }
 }
