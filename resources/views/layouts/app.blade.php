0efe3519946c94ddbccc07a591a781d621234b6d
Caen De Silva <caen@desilva.se>
2024-03-30 15:56:34 +0100

    Update title tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>{{ config('app.name', 'Windowlight - Beautiful Code Screenshots') }}</title>
+
         <link rel="canonical" href="{{ $canonical }}" />
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
