f04c7c69caae172b856f0d6d6c57921d4a4890ff
Caen De Silva <caen@desilva.se>
2024-03-30 15:26:37 +0100

    Add PHPDoc

--- app/Contracts/Torchlight.php
+++ app/Contracts/Torchlight.php
@@ -15,6 +15,11 @@ class Torchlight
      */
     public const LANGUAGES = ['abap', 'actionscript-3', 'ada', 'alpine', 'alpinejs', 'apex', 'applescript', 'asm', 'awk', 'bat', 'batch', 'c', 'csharp', 'c#', 'cpp', 'clojure', 'clj', 'cobol', 'coffee', 'crystal', 'css', 'curl', 'd', 'dart', 'dockerfile', 'elixir', 'elm', 'html-ruby-erb', 'erb', 'erlang', 'fsharp', 'f#', 'git-commit', 'diff', 'git-ignore', 'gitignore', 'git-rebase', 'gnuplot', 'go', 'graphql', 'groovy', 'hack', 'haml', 'handlebars', 'hbs', 'hcl', 'haskell', 'hlsl', 'html', 'ini', 'java', 'javascript', 'js', 'jinja-html', 'json', 'jsonc', 'jsonnet', 'jsx', 'julia', 'kotlin', 'blade', 'latex', 'tex', 'less', 'lisp', 'logo', 'lua', 'makefile', 'markdown', 'md', 'matlab', 'mdx', 'nix', 'objective-c', 'objc', 'ocaml', 'pascal', 'perl', 'perl6', 'php', '(null)', 'txt', 'text', 'plaintext', 'pls', 'postcss', 'powershell', 'ps', 'ps1', 'prolog', 'pug', 'jade', 'puppet', 'purescript', 'python', 'py', 'r', 'razor', 'ruby', 'rb', 'rust', 'sas', 'sass', 'scala', 'scheme', 'scss', 'shaderlab', 'shader', 'shell', 'smalltalk', 'sql', 'ssh-config', 'antlers', 'stylus', 'styl', 'swift', 'tcl', 'toml', 'tsx', 'typescript', 'ts', 'vb', 'cmd', 'viml', 'vue', 'wasm', '文言', 'wenyan', 'xml', 'xsl', 'yaml'];
 
+    /**
+     * Get the available languages as an HTML select options list.
+     *
+     * @return \Illuminate\Support\HtmlString
+     */
     public static function languageListOptions(): HtmlString
     {
         return new HtmlString(implode('', array_map(function (string $language): string {
