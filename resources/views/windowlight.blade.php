70215e9feae0095f11a89f9a44ad1dd4c259188d
Caen De Silva <caen@desilva.se>
2024-03-30 14:39:41 +0100

    Extract header view

--- resources/views/windowlight.blade.php
+++ resources/views/windowlight.blade.php
@@ -9,26 +9,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <header class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6 text-center">
-                <div>
-                    <h1 class="font-semibold text-2xl text-gray-800 dark:text-gray-200 leading-tight mb-4">
-                        Welcome to Windowlight.
-                    </h1>
-                    <p class="text-base text-gray-800 dark:text-gray-200">
-                        <strong>
-                            Windowlight is a simple wrapper for Torchlight, helping you to create beautiful code screenshots.
-                        </strong>
-                        <span class="block mt-1 text-sm">
-                            Because your code screenshots deserve the same love as your documentation.
-                        </span>
-                    </p>
-                    <noscript>
-                        <p class="text-sm text-red-800 dark:text-red-500 mt-2">
-                            You need to enable JavaScript to be able to download the code screenshot.
-                        </p>
-                    </noscript>
-                </div>
-            </header>
+            @include('header')
         </div>
 
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-8">
