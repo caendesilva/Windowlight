6c86c25a38884b1f116c2f024880284470bff811
Caen De Silva <caen@desilva.se>
2024-03-30 16:12:55 +0100

    Generate meta tags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,27 @@
         <title>{{ config('app.name', 'Windowlight') }} - {{ $title ?? 'Beautiful Code Screenshots' }}</title>
 
         <link rel="canonical" href="{{ $canonical }}" />
+        <meta name="description" content="Windowlight is a simple wrapper for Torchlight, helping you to create beautiful code screenshots. Because your code screenshots deserve the same love as your documentation." />
+        <meta name="author" content="Caen De Silva" />
+        <meta name="robots" content="index, follow" />
+        <meta name="keywords" content="Torchlight, Laravel, TailwindCSS, JavaScript, Code, Syntax Highlighting, Code Screenshot, Windowlight" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@CodeWithCaen" />
+        <meta name="twitter:creator" content="@CodeWithCaen" />
+        <meta name="twitter:title" content="Windowlight - Beautiful Code Screenshots" />
+        <meta name="twitter:description" content="Windowlight is a simple wrapper for Torchlight, helping you to create beautiful code screenshots. Because your code screenshots deserve the same love as your documentation." />
+        <meta name="twitter:image" content="{{ asset('og-image.png') }}" />
+        <meta property="og:url" content="{{ $canonical }}" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Windowlight - Beautiful Code Screenshots" />
+        <meta property="og:description" content="Windowlight is a simple wrapper for Torchlight, helping you to create beautiful code screenshots. Because your code screenshots deserve the same love as your documentation." />
+        <meta property="og:image" content="{{ asset('og-image.png') }}" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:site_name" content="Windowlight" />
+        <meta property="og:locale" content="{{ str_replace('_', '-', app()->getLocale()) }}" />
+        <meta property="og:locale:alternate" content="en_US" />
+        <meta property="og:locale:alternate" content="en_GB" />
 
 
         <!-- Fonts -->
