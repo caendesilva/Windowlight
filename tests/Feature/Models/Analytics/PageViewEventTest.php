d519cb1618ee30d5bf24d16cfc6b0d510fd69fee
Caen De Silva <caen@desilva.se>
2024-04-04 18:17:50 +0200

    Update test structure

--- tests/Feature/Models/Analytics/PageViewEventTest.php
+++ tests/Feature/Models/Analytics/PageViewEventTest.php
@@ -5,8 +5,8 @@ use App\Models\Analytics\PageViewEvent;
 test('can create model', function () {
     $model = PageViewEvent::factory()->create();
 
-    expect($model->id)->toBeInt();
-    expect($model->page)->toBeString();
-    expect($model->referrer)->toBeString();
-    expect($model->user_agent)->toBeString();
+    expect($model)->toBeInstanceOf(PageViewEvent::class)
+        ->and($model->exists)->toBeTrue()
+        ->and($model->id)->toBeInt()
+        ->and($model->page)->toBeString();
 });
