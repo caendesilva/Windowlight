65d7390bafbfed0eefff484248566b550fee8f4c
Caen De Silva <caen@desilva.se>
2024-04-04 20:42:09 +0200

    Create analytics.blade.php

---
+++ resources/views/analytics.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout title="Analytics">
+    <div class="py-12">
+        <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6">
+                <header class="basic-prose mx-auto text-gray-800 dark:text-gray-200 leading-tight">
+                    <h1 class="text-3xl font-bold">Public Analytics Dashboard</h1>
+                </header>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
