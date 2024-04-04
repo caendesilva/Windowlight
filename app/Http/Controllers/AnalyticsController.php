7c61a864ea224c91d872e59d0197d29e1562f474
Caen De Silva <caen@desilva.se>
2024-04-04 20:42:23 +0200

    Implement the show method

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Analytics\PageViewEvent;
 use Illuminate\Http\Request;
 
 class AnalyticsController extends Controller
 {
-    //
+    public function show(Request $request)
+    {
+        return view('analytics', [
+            'pageViews' => PageViewEvent::all()
+        ]);
+    }
 }
