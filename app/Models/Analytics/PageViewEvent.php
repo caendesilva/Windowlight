f1a7c06cedfa5f6355e41f7a160066e9ed3a453c
Caen De Silva <caen@desilva.se>
2024-04-04 18:20:33 +0200

    Add property annotations

--- app/Models/Analytics/PageViewEvent.php
+++ app/Models/Analytics/PageViewEvent.php
@@ -5,6 +5,12 @@ namespace App\Models\Analytics;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string $page
+ * @property string $referrer
+ * @property string $user_agent
+ * @property string $anonymous_id
+ */
 class PageViewEvent extends Model
 {
     use HasFactory;
