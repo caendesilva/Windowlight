bfdb78e67011df512a4475b9a836636acbcea214
Caen De Silva <caen@desilva.se>
2024-04-04 18:07:59 +0200

    Define the migration

--- database/migrations/2024_04_04_160545_create_page_view_events_table.php
+++ database/migrations/2024_04_04_160545_create_page_view_events_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('page_view_events', function (Blueprint $table) {
             $table->id();
+            $table->string('page');
+            $table->string('referrer')->nullable();
+            $table->string('user_agent')->nullable(); // Only added when the user is a bot/crawler
+            $table->string('anonymous_id')->nullable(); // Ephemeral Anonymized identifier for the user to track daily unique visits
             $table->timestamps();
         });
     }
