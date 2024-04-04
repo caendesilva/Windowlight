05613893e7e8de80847be6309b785983b7f36f78
Caen De Silva <caen@desilva.se>
2024-04-04 18:06:01 +0200

    Create PageViewEvent.php

---
+++ app/Models/Analytics/PageViewEvent.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Analytics;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PageViewEvent extends Model
+{
+    use HasFactory;
+}
