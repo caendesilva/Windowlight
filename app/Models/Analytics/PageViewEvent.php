82c67b33132c8a39f2c3ddfca68abb4a0fcea910
Caen De Silva <caen@desilva.se>
2024-04-04 18:21:45 +0200

    Add property descriptions

--- app/Models/Analytics/PageViewEvent.php
+++ app/Models/Analytics/PageViewEvent.php
@@ -6,10 +6,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * @property string $page
- * @property string $referrer
- * @property string $user_agent
- * @property string $anonymous_id
+ * @property string $page URL of the page visited
+ * @property string $referrer URL of the page that referred the user
+ * @property string $user_agent User agent string of the visitor (only stored for bots)
+ * @property string $anonymous_id Ephemeral anonymized visitor identifier that cannot be tied to a user
  */
 class PageViewEvent extends Model
 {
