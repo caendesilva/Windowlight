1ed651643db0a7c2af2bf8178febf7df4206d14a
Caen De Silva <caen@desilva.se>
2024-04-04 18:22:13 +0200

    Annotate nullable types

--- app/Models/Analytics/PageViewEvent.php
+++ app/Models/Analytics/PageViewEvent.php
@@ -7,8 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 /**
  * @property string $page URL of the page visited
- * @property string $referrer URL of the page that referred the user
- * @property string $user_agent User agent string of the visitor (only stored for bots)
+ * @property ?string $referrer URL of the page that referred the user
+ * @property ?string $user_agent User agent string of the visitor (only stored for bots)
  * @property string $anonymous_id Ephemeral anonymized visitor identifier that cannot be tied to a user
  */
 class PageViewEvent extends Model
