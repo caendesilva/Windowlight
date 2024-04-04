371f7cfd14ad1abeeedfa7e74b33d3bc8137cf1d
Caen De Silva <caen@desilva.se>
2024-04-04 21:31:17 +0200

    Seed page views with a custom distribution of dates

--- database/factories/Analytics/PageViewEventFactory.php
+++ database/factories/Analytics/PageViewEventFactory.php
@@ -23,4 +23,49 @@ class PageViewEventFactory extends Factory
             'anonymous_id' => $this->faker->sha1,
         ];
     }
+
+    public function thisYear(): static
+    {
+        return $this->state(function (array $attributes): array {
+            return [
+                'created_at' => $this->faker->dateTimeBetween('-1 year', 'now')->format('Y-m-d H:i:s')
+            ];
+        });
+    }
+
+    public function thisMonth(): static
+    {
+        return $this->state(function (array $attributes): array {
+            return [
+                'created_at' => $this->faker->dateTimeBetween('-1 month', 'now')->format('Y-m-d H:i:s')
+            ];
+        });
+    }
+
+    public function thisWeek(): static
+    {
+        return $this->state(function (array $attributes): array {
+            return [
+                'created_at' => $this->faker->dateTimeBetween('-1 week', 'now')->format('Y-m-d H:i:s')
+            ];
+        });
+    }
+
+    public function pastThreeDays(): static
+    {
+        return $this->state(function (array $attributes): array {
+            return [
+                'created_at' => $this->faker->dateTimeBetween('-3 days', 'now')->format('Y-m-d H:i:s')
+            ];
+        });
+    }
+
+    public function today(): static
+    {
+        return $this->state(function (array $attributes): array {
+            return [
+                'created_at' => $this->faker->dateTimeBetween('-1 day', 'now')->format('Y-m-d H:i:s')
+            ];
+        });
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,11 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        \App\Models\Analytics\PageViewEvent::factory(250)->create();
+        // We seed page views with a custom distribution of dates
+        \App\Models\Analytics\PageViewEvent::factory()->count(250)->thisYear()->create();
+        \App\Models\Analytics\PageViewEvent::factory()->count(100)->thisMonth()->create();
+        \App\Models\Analytics\PageViewEvent::factory()->count(50)->thisWeek()->create();
+        \App\Models\Analytics\PageViewEvent::factory()->count(25)->pastThreeDays()->create();
+        \App\Models\Analytics\PageViewEvent::factory()->count(25)->today()->create();
     }
 }
