a49d74996bd6c85f3bf8c3b3495b6181990cae15
Caen De Silva <caen@desilva.se>
2024-04-04 18:06:02 +0200

    Create PageViewEventFactory.php

---
+++ database/factories/Analytics/PageViewEventFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories\Analytics;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Analytics\PageViewEvent>
+ */
+class PageViewEventFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
