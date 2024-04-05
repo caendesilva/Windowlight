4070c6381f2e005a7996735fe625f0ad5431a664
Caen De Silva <caen@desilva.se>
2024-04-05 18:36:40 +0200

    Add default footer state

--- resources/views/components/analytics/table-navigation.blade.php
+++ resources/views/components/analytics/table-navigation.blade.php
@@ -1,9 +1,12 @@
-<tr x-show="totalPages > 1" x-cloak="">
+<tr>
     <td colspan="3" class="text-sm pt-3">
-        <div class="flex justify-between">
+        <div x-show="totalPages > 1" x-cloak="" class="flex justify-between">
             <button @click="currentPage = currentPage > 1 ? currentPage - 1 : 1">Previous</button>
             <span class="mx-2">Page <span x-text="currentPage"></span> of <span x-text="totalPages"></span></span>
             <button @click="currentPage = currentPage < totalPages ? currentPage + 1 : totalPages">Next</button>
+        </div>
+        <div x-show="totalPages == 1" x-cloak="" class="text-center opacity-75">
+            Showing <span x-text="currentPage"></span> of <span x-text="totalPages"></span> pages
         </div>
     </td>
 </tr>
