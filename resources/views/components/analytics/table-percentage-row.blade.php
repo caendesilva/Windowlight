998a2872cecacfe26002d50a284092dd5cb786e8
Caen De Silva <caen@desilva.se>
2024-04-05 20:45:54 +0200

    Add dark mode support
    Colors are still based on the Charts.js theme

--- resources/views/components/analytics/table-percentage-row.blade.php
+++ resources/views/components/analytics/table-percentage-row.blade.php
@@ -1,5 +1,5 @@
 @props(['percentage', 'label', 'scale' => 2])
 
-<div class="whitespace-nowrap rounded px-2 mb-1 bg-[#D9EDFC] group-hover:bg-[#B9DEF9]" style="width: {{ round($percentage * $scale) }}%; min-width: {{ round(8 - $scale) }}%; max-width: 50vw;">
+<div class="whitespace-nowrap rounded px-2 mb-1 bg-[#D9EDFC] group-hover:bg-[#B9DEF9] dark:bg-[#26415B] dark:group-hover:bg-[#2A597F]" style="width: {{ round($percentage * $scale) }}%; min-width: {{ round(8 - $scale) }}%; max-width: 50vw;">
     {{ $label }}
 </div>
