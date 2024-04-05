b3eab971d43162ffc51dede68489e5297f58dd0b
Caen De Silva <caen@desilva.se>
2024-04-05 15:10:04 +0200

    Use cleaner domain names

--- app/Models/Analytics/PageViewEvent.php
+++ app/Models/Analytics/PageViewEvent.php
@@ -37,7 +37,7 @@ class PageViewEvent extends Model
 
         static::creating(function (self $model): void {
             // We only store the domain of the referrer
-            $model->referrer = $model->referrer ? parse_url($model->referrer, PHP_URL_HOST) : null;
+            $model->referrer = $model->referrer ? Str::after(parse_url($model->referrer, PHP_URL_HOST), 'www.') : null;
 
             // We don't store user agents for non-bot users
             $crawlerKeywords = ['bot', 'crawl', 'spider', 'slurp', 'search', 'yahoo', 'facebook'];
