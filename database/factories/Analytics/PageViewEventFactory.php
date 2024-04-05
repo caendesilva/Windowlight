16028b30cccf0a5b57f6be7af7c17eb1cf018620
Caen De Silva <caen@desilva.se>
2024-04-05 15:35:49 +0200

    Lower chance of being unknown

--- database/factories/Analytics/PageViewEventFactory.php
+++ database/factories/Analytics/PageViewEventFactory.php
@@ -35,7 +35,7 @@ class PageViewEventFactory extends Factory
 
     public function getReferrer(): ?string
     {
-        $chanceOfBeingUnknown = 50;
+        $chanceOfBeingUnknown = 40;
 
         if ($this->faker->boolean($chanceOfBeingUnknown)) {
             return null;
