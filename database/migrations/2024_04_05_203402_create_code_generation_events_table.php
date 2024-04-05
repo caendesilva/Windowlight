71cec5c2c5a517fc8545be3e716350e07d65400c
Caen De Silva <caen@desilva.se>
2024-04-05 22:38:46 +0200

    Define the migration

--- database/migrations/2024_04_05_203402_create_code_generation_events_table.php
+++ database/migrations/2024_04_05_203402_create_code_generation_events_table.php
@@ -13,7 +13,14 @@ return new class extends Migration
     {
         Schema::create('code_generation_events', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('language');
+            $table->boolean('hasMenubar');
+            $table->boolean('hasLineNumbers');
+            $table->boolean('hasMenuButtons');
+            $table->boolean('hasMenubarText');
+            $table->string('background');
+            $table->integer('lines');
+            $table->timestamp('created_at')->nullable();
         });
     }
 
