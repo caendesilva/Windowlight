6830b80e05a0eb4089c85e679c6b957225ec5bb3
Caen De Silva <caen@desilva.se>
2024-04-06 11:42:58 +0200

    Refactor shared markup to simple layout view

--- resources/views/markdown.blade.php
+++ resources/views/markdown.blade.php
@@ -1,11 +1,7 @@
-<x-app-layout :title="$title">
-    <div class="py-12">
-        <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
-            <main class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6">
-                <article class="basic-prose mx-auto text-gray-800 dark:text-gray-200 leading-tight">
-                    {{ $markdown }}
-                </article>
-            </main>
-        </div>
-    </div>
-</x-app-layout>
+<x-simple-layout :title="$title">
+    <main class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg p-6">
+        <article class="basic-prose mx-auto text-gray-800 dark:text-gray-200 leading-tight">
+            {{ $markdown }}
+        </article>
+    </main>
+</x-simple-layout>
