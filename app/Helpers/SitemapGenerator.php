3dcb65631fb192024e3eb87ddc7b974c9569511d
Caen De Silva <caen@desilva.se>
2024-04-06 10:53:58 +0200

    Copy over parent method

--- app/Helpers/SitemapGenerator.php
+++ app/Helpers/SitemapGenerator.php
@@ -2,9 +2,18 @@
 
 namespace App\Helpers;
 
+use Hyde\Foundation\Facades\Routes;
 use Hyde\Framework\Features\XmlGenerators\SitemapGenerator as HydeSitemapGenerator;
+use Hyde\Support\Models\Route;
 
 class SitemapGenerator extends HydeSitemapGenerator
 {
-    //
+    public function generate(): static
+    {
+        Routes::all()->each(function (Route $route): void {
+            $this->addRoute($route);
+        });
+
+        return $this;
+    }
 }
