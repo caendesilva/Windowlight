b033b342056bd59c2729d267e17b27820e875e56
Caen De Silva <caen@desilva.se>
2024-04-06 11:38:53 +0200

    Create simple.blade.php

---
+++ resources/views/layouts/simple.blade.php
@@ -0,0 +1,7 @@
+<x-app-layout :title="$title">
+    <div class="py-12">
+        <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
+            {{ $slot }}
+        </div>
+    </div>
+</x-app-layout>
