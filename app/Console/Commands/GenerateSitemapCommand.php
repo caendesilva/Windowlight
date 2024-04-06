ff227581bad7126bbdf561c8f51089cd6890024f
Caen De Silva <caen@desilva.se>
2024-04-06 10:43:27 +0200

    Mix in routes when generating sitemap

--- app/Console/Commands/GenerateSitemapCommand.php
+++ app/Console/Commands/GenerateSitemapCommand.php
@@ -3,9 +3,13 @@
 namespace App\Console\Commands;
 
 use App\Providers\HydeServiceProvider;
+use Hyde\Foundation\Facades\Routes;
 use Hyde\Framework\Actions\PostBuildTasks\GenerateRssFeed;
 use Hyde\Framework\Actions\PostBuildTasks\GenerateSitemap;
+use Hyde\Pages\InMemoryPage;
+use Hyde\Support\Models\Route as HydeRoute;
 use Illuminate\Console\Command;
+use Illuminate\Routing\Route;
 
 /**
  * @see \Hyde\Console\Commands\BuildRssFeedCommand
@@ -21,6 +25,22 @@ class GenerateSitemapCommand extends Command
     public function handle(): int
     {
         app()->register(HydeServiceProvider::class);
+
+        // Mix in the Laravel routes into the Hyde routes
+        $routes = [
+            'home',
+            'about',
+            'examples',
+            'analytics',
+            'analytics.raw',
+            'analytics.json',
+        ];
+
+        foreach ($routes as $name) {
+            $route = app('router')->getRoutes()->getByName($name);
+            $hydeRoute = new HydeRoute(new InMemoryPage($route->uri));
+            Routes::addRoute($hydeRoute);
+        }
 
         return (new GenerateSitemap())->run($this->output);
     }
