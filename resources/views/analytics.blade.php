e1de203937da7bd423b4c38beed0d517ec18f0ef
Caen De Silva <caen@desilva.se>
2024-04-06 14:16:15 +0200

    Remove scale options
    They are invalid and unused

--- resources/views/analytics.blade.php
+++ resources/views/analytics.blade.php
@@ -218,15 +218,6 @@
                             borderColor: 'rgba(54, 162, 235, 1)',
                             borderWidth: 1
                         }]
-                    },
-                    options: {
-                        scales: {
-                            xAxes: [{
-                                ticks: {
-                                    beginAtZero: true
-                                }
-                            }]
-                        }
                     }
                 });
             });
