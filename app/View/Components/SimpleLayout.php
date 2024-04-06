2f45e48b617441655d223644ebe7d2815670c15c
Caen De Silva <caen@desilva.se>
2024-04-06 11:56:29 +0200

    Add footer to simple layout

--- app/View/Components/SimpleLayout.php
+++ app/View/Components/SimpleLayout.php
@@ -7,9 +7,17 @@ use Illuminate\View\View;
 
 class SimpleLayout extends Component
 {
+    /** @var array<string, string> */
+    public array $footer;
+
     public function __construct(public ?string $title = null)
     {
-        //
+        $this->footer = [
+            'about' => 'About',
+            'posts' => 'Blog',
+            'privacy-policy' => 'Privacy Policy',
+            'terms-of-service' => 'Terms of Service',
+        ];
     }
 
     /**
--- resources/views/layouts/simple.blade.php
+++ resources/views/layouts/simple.blade.php
@@ -4,4 +4,20 @@
             {{ $slot }}
         </div>
     </div>
+    <footer>
+        <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
+            <div class="border-t border-gray-200 dark:border-gray-700 py-4 flex justify-between items-center">
+                <p class="text-sm text-gray-600 dark:text-gray-400">
+                    &copy; {{ date('Y') }} {{ config('app.name') }}. All rights reserved.
+                </p>
+                <ul class="flex text-sm text-gray-600 dark:text-gray-400">
+                    @foreach($footer as $route => $label)
+                        <li class="px-1">
+                            <a href="{{ route($route) }}" class="hover:text-gray-900 dark:hover:text-gray-100">{{ $label }}</a>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    </footer>
 </x-app-layout>
