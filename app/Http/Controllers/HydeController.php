259bdd7d764b0ee0438d536a54a78002a98957e9
Caen De Silva <caen@desilva.se>
2024-04-06 09:47:09 +0200

    Render custom posts page

--- app/Http/Controllers/HydeController.php
+++ app/Http/Controllers/HydeController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 use Hyde\Pages\MarkdownPost;
+use Hyde\Pages\InMemoryPage;
 
 class HydeController extends Controller
 {
     public function posts(): View
     {
-        return view('hyde::components.blog-post-feed', [
+        return view('hyde.posts', [
+            'page' => new InMemoryPage('posts'),
             'posts' => MarkdownPost::getLatestPosts(),
         ]);
     }
