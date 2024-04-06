5b97705c1eaaaa117072b3b3db9d3ce42bc1c1a0
Caen De Silva <caen@desilva.se>
2024-04-06 10:13:57 +0200

    Create posts layout

--- resources/views/hyde/post.blade.php
+++ resources/views/hyde/post.blade.php
@@ -1,1 +1,9 @@
-@include('hyde::components.post.article')
+<x-app-layout :title="$page->title">
+    <div class="py-12">
+        <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
+            <main id="content" class="mx-auto max-w-7xl py-16 px-8">
+                @include('hyde::components.post.article')
+            </main>
+        </div>
+    </div>
+</x-app-layout>
