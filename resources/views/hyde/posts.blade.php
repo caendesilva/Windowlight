5dec43fd3d81223a18e8a7ead1926528094fd8ca
Caen De Silva <caen@desilva.se>
2024-04-06 11:43:43 +0200

    Lower padding

--- resources/views/hyde/posts.blade.php
+++ resources/views/hyde/posts.blade.php
@@ -1,5 +1,5 @@
 <x-simple-layout :title="$page->title">
-    <main id="content" class="mx-auto max-w-7xl py-12 px-8">
+    <main id="content" class="mx-auto max-w-7xl py-8 px-8">
         <header class="lg:mb-12 xl:mb-16">
             <h1 class="text-3xl text-left leading-10 tracking-tight font-extrabold sm:leading-none mb-8 md:mb-12 md:text-center md:text-4xl text-gray-700 dark:text-gray-200">
                 Latest Posts
