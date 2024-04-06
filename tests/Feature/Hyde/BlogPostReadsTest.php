36a7ab4261e2d9ba6c63e8a3d4fbca0e34da5dfd
Caen De Silva <caen@desilva.se>
2024-04-06 13:28:33 +0200

    Test can get reads for a specific post

--- tests/Feature/Hyde/BlogPostReadsTest.php
+++ tests/Feature/Hyde/BlogPostReadsTest.php
@@ -14,3 +14,14 @@ test('can get reads', function () {
         'posts/baz' => 1,
     ]);
 });
+
+test('can get reads for a specific post', function () {
+    PageViewEvent::factory()->count(5)->create(['page' => 'posts/foo']);
+    PageViewEvent::factory()->count(3)->create(['page' => 'posts/bar']);
+    PageViewEvent::factory()->count(1)->create(['page' => 'posts/baz']);
+
+    expect(BlogPostReads::get('foo'))->toBe(5);
+    expect(BlogPostReads::get('bar'))->toBe(3);
+    expect(BlogPostReads::get('baz'))->toBe(1);
+    expect(BlogPostReads::get('unknown'))->toBe(0);
+});
