3a1c609cc5ba6f6ba9a8922b1dea598ad18617fc
Caen De Silva <caen@desilva.se>
2024-04-06 09:30:24 +0200

    Register facade aliases to use in the views

--- app/Providers/HydeServiceProvider.php
+++ app/Providers/HydeServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Hyde\Foundation\HydeKernel;
+use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\ServiceProvider;
 
 class HydeServiceProvider extends ServiceProvider
@@ -19,6 +20,11 @@ class HydeServiceProvider extends ServiceProvider
 
             return $kernel;
         });
+
+        // Register facade aliases to use in the views
+        $loader = AliasLoader::getInstance();
+        $loader->alias('Hyde', \Hyde\Hyde::class);
+        $loader->alias('MarkdownPost', \Hyde\Pages\MarkdownPost::class);
 
         // Define the view hint path for the Hyde package
         $this->loadViewsFrom(base_path('vendor/hyde/framework/resources/views'), 'hyde');
