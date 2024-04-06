6ebab406b503e225ddb1dad4b0c0e87ab37d0d83
Caen De Silva <caen@desilva.se>
2024-04-06 10:54:55 +0200

    Sort routes to put blog posts last

--- app/Helpers/SitemapGenerator.php
+++ app/Helpers/SitemapGenerator.php
@@ -4,6 +4,7 @@ namespace App\Helpers;
 
 use Hyde\Foundation\Facades\Routes;
 use Hyde\Framework\Features\XmlGenerators\SitemapGenerator as HydeSitemapGenerator;
+use Hyde\Pages\MarkdownPost;
 use Hyde\Support\Models\Route;
 
 class SitemapGenerator extends HydeSitemapGenerator
@@ -11,6 +12,9 @@ class SitemapGenerator extends HydeSitemapGenerator
     public function generate(): static
     {
         $routes = Routes::all();
+
+        // Sort routes to put blog posts last
+        $routes = $routes->sortBy(fn (Route $route): int => $route->getPage() instanceof MarkdownPost ? 1 : 0);
 
         $routes->each(function (Route $route): void {
             $this->addRoute($route);
