ad18147f99782bdefe0e0b8c926319c5353317a1
Caen De Silva <caen@desilva.se>
2024-04-06 10:24:58 +0200

    Create GenerateRssFeedCommand.php

---
+++ app/Console/Commands/GenerateRssFeedCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class GenerateRssFeedCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:generate-rss-feed-command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
