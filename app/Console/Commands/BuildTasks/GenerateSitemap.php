a6e180374702e7bf44ba796409cc34f99fac2cd1
Caen De Silva <caen@desilva.se>
2024-04-06 10:52:52 +0200

    Create GenerateSitemap.php

---
+++ app/Console/Commands/BuildTasks/GenerateSitemap.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Console\Commands\BuildTasks;
+
+use Hyde\Framework\Actions\PostBuildTasks\GenerateSitemap as HydeGenerateSitemap;
+
+class GenerateSitemap extends HydeGenerateSitemap
+{
+    //
+}
