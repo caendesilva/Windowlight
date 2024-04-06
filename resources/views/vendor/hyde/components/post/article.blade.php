aacdf76b49ca833dcb1b800a4f82683cec91a472
Caen De Silva <caen@desilva.se>
2024-04-06 16:24:57 +0200

    Publish article.blade.php

---
+++ resources/views/vendor/hyde/components/post/article.blade.php
@@ -0,0 +1,22 @@
+<article aria-label="Article" id="{{ $page->identifier }}" itemscope
+         itemtype="https://schema.org/Article"
+        @class(['post-article mx-auto', config('markdown.prose_classes', 'prose dark:prose-invert'), 'torchlight-enabled' => Features::hasTorchlight()])>
+    <meta itemprop="identifier" content="{{ $page->identifier }}">
+    @if($page->getCanonicalUrl() !== null)
+        <meta itemprop="url" content="{{ $page->getCanonicalUrl() }}">
+    @endif
+
+    <header aria-label="Header section" role="doc-pageheader">
+        <h1 itemprop="headline" class="mb-4">{{ $page->title }}</h1>
+        <div id="byline" aria-label="About the post" role="doc-introduction">
+            @includeWhen(isset($page->date), 'hyde::components.post.date')
+            @includeWhen(isset($page->author), 'hyde::components.post.author')
+            @includeWhen(isset($page->category), 'hyde::components.post.category')
+        </div>
+    </header>
+    @includeWhen(isset($page->image), 'hyde::components.post.image')
+    <div aria-label="Article body" itemprop="articleBody">
+        {{ $content }}
+    </div>
+    <span class="sr-only">End of article</span>
+</article>
