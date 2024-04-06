cd564dded5f6ed352939fe7840f2a38c3d6d8c76
Caen De Silva <caen@desilva.se>
2024-04-06 10:50:44 +0200

    Create SitemapGenerator.php

---
+++ app/Helpers/SitemapGenerator.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Helpers;
+
+use Hyde\Framework\Features\XmlGenerators\SitemapGenerator as HydeSitemapGenerator;
+
+class SitemapGenerator extends HydeSitemapGenerator
+{
+    //
+}
