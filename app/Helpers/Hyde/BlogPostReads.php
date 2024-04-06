2d25072e709c7f9d4554482d450855dc1aa34204
Caen De Silva <caen@desilva.se>
2024-04-06 13:19:59 +0200

    Sketch out class API

--- app/Helpers/Hyde/BlogPostReads.php
+++ app/Helpers/Hyde/BlogPostReads.php
@@ -6,4 +6,25 @@ class BlogPostReads
 {
     /** @var array<string, int> */
     protected static $reads = [];
+
+    public static function all(): array
+    {
+        static::loadIfEmpty();
+
+        return static::$reads;
+    }
+
+    public static function get(string $identifier): int
+    {
+        static::loadIfEmpty();
+
+        return static::$reads[$identifier];
+    }
+
+    protected static function loadIfEmpty(): void
+    {
+        if (empty(static::$reads)) {
+            static::load();
+        }
+    }
 }
