fe4a98efed0188bcf2356b930d3c0663206b542f
Caen De Silva <caen@desilva.se>
2024-04-06 10:49:53 +0200

    Revert "Experiment to replace kernel to put posts last"
    This reverts commit 956606c2d0939cd552c3a88f950d4ec0f151d59a.

--- app/Console/Commands/GenerateSitemapCommand.php
+++ app/Console/Commands/GenerateSitemapCommand.php
@@ -4,10 +4,8 @@ namespace App\Console\Commands;
 
 use App\Providers\HydeServiceProvider;
 use Hyde\Foundation\Facades\Routes;
-use Hyde\Foundation\HydeKernel;
 use Hyde\Framework\Actions\PostBuildTasks\GenerateRssFeed;
 use Hyde\Framework\Actions\PostBuildTasks\GenerateSitemap;
-use Hyde\Hyde;
 use Hyde\Pages\InMemoryPage;
 use Hyde\Support\Models\Route as HydeRoute;
 use Illuminate\Console\Command;
@@ -27,14 +25,6 @@ class GenerateSitemapCommand extends Command
     public function handle(): int
     {
         app()->register(HydeServiceProvider::class);
-
-        $automaticallyAddedRoutes = Hyde::routes()->all();
-
-        // Add a fresh kernel so that we can put the post routes last
-        $hyde = new HydeKernel(base_path());
-        $hyde->setSourceRoot('/dev/null');
-        $hyde->setOutputDirectory('public');
-        HydeKernel::setInstance($hyde);
 
         // Mix in the Laravel routes into the Hyde routes
         $routes = [
