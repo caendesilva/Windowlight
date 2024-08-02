6b2c09aaae4a7103032f402c03550002a55dd267
Caen De Silva <caen@desilva.se>
2024-08-02 18:47:14 +0200

    Tweak spacing

--- resources/views/windowlight.blade.php
+++ resources/views/windowlight.blade.php
@@ -54,9 +54,9 @@
                                             <x-input-error :messages="$errors->get('headerText')" class="mt-2" />
                                         </div>
                                         <div class="mb-4">
-                                            <div class="flex justify-between items-center mb-2">
+                                            <div class="flex justify-between items-end">
                                                 <x-input-label for="background" value="Background color" />
-                                                <button type="button" id="colorPresetsToggle" class="requires-javascript text-gray-600 hover:text-gray-800 dark:text-gray-300 dark:hover:text-gray-100 relative" title="Choose preset color">
+                                                <button type="button" id="colorPresetsToggle" class="requires-javascript text-gray-600 hover:text-gray-800 dark:text-gray-300 dark:hover:text-gray-100 relative mb-1" title="Choose preset color">
                                                     <span class="sr-only">Choose preset color</span>
                                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                                         <path fill-rule="evenodd" d="M4 2a2 2 0 00-2 2v14a2 2 0 002 2h12a2 2 0 002-2V4a2 2 0 00-2-2H4zm1 2h10a1 1 0 011 1v10a1 1 0 01-1 1H5a1 1 0 01-1-1V5a1 1 0 011-1zm7 3a1 1 0 11-2 0 1 1 0 012 0zm-3 1a1 1 0 100 2 1 1 0 000-2zm-3 3a1 1 0 110 2 1 1 0 010-2zm7-4a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd" />
