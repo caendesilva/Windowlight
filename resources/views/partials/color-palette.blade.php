2a3ed27046ac410a248ba332b3e4f06e34f82bf3
Caen De Silva <caen@desilva.se>
2024-08-06 10:17:56 +0200

    Semantic HTML

--- resources/views/partials/color-palette.blade.php
+++ resources/views/partials/color-palette.blade.php
@@ -1,5 +1,5 @@
-<div class="font-sans flex flex-row items-center w-fit m-2 p-2 border border-gray-300">
+<section class="color-palette font-sans flex flex-row items-center w-fit m-2 p-2 border border-gray-300">
     @foreach(\App\Helpers\ColorHelper::getBackgroundColors() as $name => $hex)
         <figure class="w-16 h-16 m-2 p-4 border border-gray-300" style="background: {{ $hex }};" title="{{ $name }}"></figure>
     @endforeach
-</div>
+</section>
