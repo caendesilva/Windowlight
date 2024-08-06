b5116df1768743339fca486e59647a56f163f832
Caen De Silva <caen@desilva.se>
2024-08-06 10:22:03 +0200

    Use gap instead of margins

--- resources/views/partials/color-palette.blade.php
+++ resources/views/partials/color-palette.blade.php
@@ -1,6 +1,6 @@
-<section class="color-palette font-sans flex flex-row items-center w-fit m-2 p-2 border border-gray-300">
+<section class="color-palette font-sans flex flex-row items-center w-fit m-4 p-4 gap-4 border border-gray-300">
     @foreach(\App\Helpers\ColorHelper::getBackgroundColors() as $name => $hex)
-        <figure class="w-16 h-16 m-2 border border-gray-300" style="background: {{ $hex }};" title="{{ \Hyde\Foundation\HydeKernel::makeTitle($name) }}"></figure>
+        <figure class="w-16 h-16 border border-gray-300" style="background: {{ $hex }};" title="{{ \Hyde\Foundation\HydeKernel::makeTitle($name) }}"></figure>
     @endforeach
 </section>
 <script>
