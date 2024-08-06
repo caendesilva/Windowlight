6c6889372c8adf8ac7b673293a428f21fe5a863f
Caen De Silva <caen@desilva.se>
2024-08-06 10:12:23 +0200

    Create color-palette.blade.php

---
+++ resources/views/partials/color-palette.blade.php
@@ -0,0 +1,10 @@
+<div style="font-family: sans-serif;display: flex; flex-direction: row; align-items: center; width: fit-content; margin: 0.5rem; padding: 0.5rem; border: 1px solid gray">
+    @foreach(\App\Helpers\ColorHelper::getBackgroundColors() as $name => $hex)
+        <figure style="background: {{ $hex }}; width: 4rem; height: 4rem; margin: 0.5rem; padding: 1rem; display: block; border: 1px solid gray;">
+            <figcaption style="font-size: 1rem; margin: 0; padding: 0; text-align: center; display: flex; flex-direction: column; justify-content: space-between; height: 100%; width: 100%; {{ \App\Helpers\ColorHelper::isColorDark($hex) ? 'color: white;' : 'color: black;' }}">
+                <header>{{ $name }}</header>
+                <footer>{{ $hex }}</footer>
+            </figcaption>
+        </figure>
+    @endforeach
+</div>
