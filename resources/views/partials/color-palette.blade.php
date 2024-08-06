d6924b37f6f18fc71b3570b90860e94632dc71a3
Caen De Silva <caen@desilva.se>
2024-08-06 10:15:04 +0200

    Replace figcaption with title

--- resources/views/partials/color-palette.blade.php
+++ resources/views/partials/color-palette.blade.php
@@ -1,10 +1,5 @@
 <div class="font-sans flex flex-row items-center w-fit m-2 p-2 border border-gray-300">
     @foreach(\App\Helpers\ColorHelper::getBackgroundColors() as $name => $hex)
-        <figure class="w-16 h-16 m-2 p-4 border border-gray-300" style="background: {{ $hex }};">
-            <figcaption class="text-base m-0 p-0 text-center flex flex-col justify-between h-full w-full {{ \App\Helpers\ColorHelper::isColorDark($hex) ? 'text-white' : 'text-black' }}">
-                <header>{{ $name }}</header>
-                <footer>{{ $hex }}</footer>
-            </figcaption>
-        </figure>
+        <figure class="w-16 h-16 m-2 p-4 border border-gray-300" style="background: {{ $hex }};" title="{{ $name }}"></figure>
     @endforeach
 </div>
